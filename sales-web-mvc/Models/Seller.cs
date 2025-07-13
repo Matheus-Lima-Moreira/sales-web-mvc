9b93b60f2678ccba3ac37309bf3cd98088ec4ea5
mattsz <6matheuslima@gmail.com>
2025-07-13 11:52:53 -0300

    :sparkles: feat: validations

--- sales-web-mvc/Models/Seller.cs
+++ sales-web-mvc/Models/Seller.cs
@@ -5,18 +5,32 @@ namespace sales_web_mvc.Models;
 public class Seller
 {
   public int Id { get; set; }
+
+  [Required(ErrorMessage = "{0} is required.")]
+  [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} must be between {2} and {1} characters.")]
   public string Name { get; set; } = "";
+
+  [Required(ErrorMessage = "{0} is required.")]
+  [EmailAddress(ErrorMessage = "Enter a valid {0}.")]
   [DataType(DataType.EmailAddress)]
   public string Email { get; set; } = "";
+
+  [Required(ErrorMessage = "{0} is required.")]
+  [Range(100.0, 50000.0, ErrorMessage = "{0} must be between {1} and {2}.")]
   [Display(Name = "Base Salary")]
   [DisplayFormat(DataFormatString = "{0:F2}")]
   public double BaseSalary { get; set; }
+
+  [Required(ErrorMessage = "{0} is required.")]
   [Display(Name = "Birth Date")]
   [DataType(DataType.Date)]
   public DateTime BirthDate { get; set; }
+
   public Department Department { get; set; } = null!;
+
   [Display(Name = "Department")]
   public int DepartmentId { get; set; }
+
   public ICollection<SalesRecord> Sales { get; set; } = [];
 
   public Seller()
